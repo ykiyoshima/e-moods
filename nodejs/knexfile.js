7eba9253fcb9c3a616dee2545f8a89bac8549e4c
ykiyoshima <y.kiyoshima.271@gmail.com>
2022-03-26 20:30:09 +0900

    knex deploy test

--- nodejs/knexfile.js
+++ nodejs/knexfile.js
@@ -1,3 +1,8 @@
+// Update with your config settings.
+
+/**
+ * @type { Object.<string, import("knex").Knex.Config> }
+ */
 import dotenv from "dotenv";
 
 dotenv.config();
@@ -8,39 +13,51 @@ const knexfile = {
     client: "mysql",
     connection: {
       database: process.env.DB_NAME,
-      user: process.env.DB_USER,
-      password: process.env.DB_PASS,
+      user: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
     },
     pool: {
       min: 2,
       max: 10
     },
+    migrations: {
+      directory:'./db/migrations',
+      tableName: 'knex_migrations'
+    }
   },
 
   staging: {
     client: "mysql",
     connection: {
       database: process.env.DB_NAME,
-      user: process.env.DB_USER,
-      password: process.env.DB_PASS,
+      user: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
     },
     pool: {
       min: 2,
       max: 10
     },
+    migrations: {
+      directory:'./db/migrations',
+      tableName: 'knex_migrations'
+    }
   },
 
   production: {
     client: "mysql",
     connection: {
       database: process.env.DB_NAME,
-      user: process.env.DB_USER,
-      password: process.env.DB_PASS,
+      user: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
     },
     pool: {
       min: 2,
       max: 10
     },
+    migrations: {
+      directory:'./db/migrations',
+      tableName: 'knex_migrations'
+    }
   }
 
 };
