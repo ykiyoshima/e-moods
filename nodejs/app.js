49082f9c2808f49d69c34d8e72d14c92a5cc489d
ykiyoshima <y.kiyoshima.271@gmail.com>
2022-03-30 09:38:25 +0900

    usertest 04

--- nodejs/app.js
+++ nodejs/app.js
@@ -134,12 +134,13 @@ app.post('/regist', (req, res, next) => {
       .then(() => {
         res.redirect('/');
       });
+  } else {
+    myknex('users')
+      .insert({ 'username': sessionUsername, 'email': sessionEmail, 'password': sessionPassword, 'favorite_id_1': idsArray[0], 'favorite_id_2': idsArray[1], 'favorite_id_3': idsArray[2], 'favorite_id_4': idsArray[3], 'favorite_id_5': idsArray[4], 'created_at': new Date(new Date().toLocaleString({ timeZone: 'Asia/Tokyo' })), 'updated_at': new Date(new Date().toLocaleString({ timeZone: 'Asia/Tokyo' })) })
+      .then(() => {
+        res.end();
+      });
   }
-  myknex('users')
-    .insert({ 'username': sessionUsername, 'email': sessionEmail, 'password': sessionPassword, 'favorite_id_1': idsArray[0], 'favorite_id_2': idsArray[1], 'favorite_id_3': idsArray[2], 'favorite_id_4': idsArray[3], 'favorite_id_5': idsArray[4], 'created_at': new Date(new Date().toLocaleString({ timeZone: 'Asia/Tokyo' })), 'updated_at': new Date(new Date().toLocaleString({ timeZone: 'Asia/Tokyo' })) })
-    .then(() => {
-      res.end();
-    });
 });
 
 app.get('/first_get_token', async (req, res) => {
