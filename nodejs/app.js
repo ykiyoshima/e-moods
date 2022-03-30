3a5116dfa9faa4ace612d11e7a0a28841adf726d
ykiyoshima <y.kiyoshima.271@gmail.com>
2022-03-30 09:10:06 +0900

    usertest 04

--- nodejs/app.js
+++ nodejs/app.js
@@ -127,6 +127,13 @@ app.post('/regist', (req, res, next) => {
   const { ids } = req.body;
   const idsArray = ids.split(',');
 
+  if (req.user) {
+    myknex('users')
+    .update({ 'favorite_id_1': idsArray[0], 'favorite_id_2': idsArray[1], 'favorite_id_3': idsArray[2], 'favorite_id_4': idsArray[3], 'favorite_id_5': idsArray[4], 'updated_at': new Date(new Date().toLocaleString({ timeZone: 'Asia/Tokyo' })) })
+    .then(() => {
+      res.end();
+    });
+  }
   myknex('users')
     .insert({ 'username': sessionUsername, 'email': sessionEmail, 'password': sessionPassword, 'favorite_id_1': idsArray[0], 'favorite_id_2': idsArray[1], 'favorite_id_3': idsArray[2], 'favorite_id_4': idsArray[3], 'favorite_id_5': idsArray[4], 'created_at': new Date(new Date().toLocaleString({ timeZone: 'Asia/Tokyo' })), 'updated_at': new Date(new Date().toLocaleString({ timeZone: 'Asia/Tokyo' })) })
     .then(() => {
