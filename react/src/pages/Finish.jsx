ec4a13190284d263e67cc4835a0c24e549ea8718
ykiyoshima <y.kiyoshima.271@gmail.com>
2022-03-30 17:16:47 +0900

    細かい微調整

--- react/src/pages/Finish.jsx
+++ react/src/pages/Finish.jsx
@@ -17,7 +17,7 @@ export const Finish = ({ title }) => {
     <div id="main" className="sm:w-full md:w-1/3 mx-auto">
       <h1 className="text-3xl font-bold pt-24 pb-16">{title}</h1>
       <h3>初期設定が完了しました！</h3>
-      <p>3秒後にログインページへ移動します</p>
+      <p>3秒後に自動でページ遷移します</p>
     </div>
   );
 };
--- react/src/pages/Index.jsx
+++ react/src/pages/Index.jsx
@@ -109,7 +109,7 @@ export const Index = () => {
       <h1 className="text-5xl font-bold pt-24 pb-16">e-moods</h1>
       <p><span id="username"></span>の顔写真を送信することで<br/>写真から感情を分析しその結果に基づいて<br/>あなたにぴったりな3曲を選びます！</p>
       <div id="signin_btn" className="my-8"></div>
-      <button className="bg-green-500 rounded-lg w-48 py-2 px-4" onClick={() => moveToSetting()}>好きなアーティスト設定</button>
+      <button className="bg-green-500 rounded-lg w-48 py-2 px-4" onClick={() => moveToSetting()}>設定アーティスト変更</button>
       <div {...getRootProps({ style })}>
         <input {...getInputProps()} />
         <FontAwesomeIcon className="text-6xl mt-6 mb-4" icon={faImage} />
