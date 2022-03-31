f6f8a11d28b3a494517c89aac6e7d18c33233cf9
ykiyoshima <y.kiyoshima.271@gmail.com>
2022-03-31 15:26:49 +0900

    Signup.jsx modified

--- react/src/pages/Signup.jsx
+++ react/src/pages/Signup.jsx
@@ -23,17 +23,9 @@ export const Signup = ({ title }) => {
   return (
     <div id="main" className="sm:w-full md:w-1/3 mx-auto">
       <h1 className="text-3xl font-bold pt-24 pb-16">{title}</h1>
-      <label htmlFor="username">ユーザー名</label><br/>
-      <input type="text" required id="username" className="text-gray-900 rounded-md px-2 mb-4" /><br />
 
       <label htmlFor="email">メールアドレス</label><br/>
       <input type="email" required id="email" className="text-gray-900 rounded-md px-2 mb-4" /><br />
-
-      <label htmlFor="password">パスワード</label><br/>
-      <input type="password" required id="password" className="text-gray-900 rounded-md px-2 mb-4" /><br />
-
-      <label htmlFor="repassword">パスワード再入力</label><br/>
-      <input type="password" required id="repassword" className="text-gray-900 rounded-md px-2 mb-4" /><br />
 
       <p id="message"></p>
       <button className="bg-green-500 rounded-lg w-48 py-2 px-4 mt-4" onClick={() => execSignup()}>次へ進む</button>
