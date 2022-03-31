0bd39b7c9c8589dfee8a5b2615fde0ac358aa123
ykiyoshima <y.kiyoshima.271@gmail.com>
2022-03-31 13:24:22 +0900

    build to public

--- nodejs/app.js
+++ nodejs/app.js
@@ -56,7 +56,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(cookieParser());
 app.use(fileUpload());
-app.use(express.static(path.join(__dirname, './react/build')));
+app.use(express.static(path.join(__dirname, './react/public')));
 app.use(flash());
 
 const sess = {
@@ -301,9 +301,9 @@ app.get('/insert_emotions_and_tracks', (req, res) => {
 app.get('*', (req, res) => {
   console.log(req.user);
   if (req.user) {
-    res.sendFile(path.join(__dirname,'./react/build/index.html'));
+    res.sendFile(path.join(__dirname,'./react/public/index.html'));
   } else {
-    res.render('/login');
+    res.redirect('/login');
   }
 });
 
