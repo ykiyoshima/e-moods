9b7d5daf0dc916e611588112746377759c35d480
ykiyoshima <y.kiyoshima.271@gmail.com>
2022-04-01 16:52:24 +0900

    tweet added

--- react/src/pages/Imported.jsx
+++ react/src/pages/Imported.jsx
@@ -14,11 +14,20 @@ export const Imported = () => {
       document.getElementById('playlist_result').innerHTML = playlistTagArray;
     });
 
+  const tweet = () => {
+    const baseUrl = 'https://twitter.com/intent/tweet?';
+    const text = ['text', 'ツイート本文'];
+    const hashtags = ['hashtags', ['ハッシュタグ1', 'ハッシュタグ2'].join(',')];
+    const query = new URLSearchParams([text, hashtags]).toString();
+    window.location.href = `${baseUrl}${query}`;
+  };
+
   return (
     <div id="main" className="sm:w-full md:w-1/3 mx-auto">
       <h1 className="text-3xl font-bold pt-24 pb-16">インポート完了</h1>
       <p>選ばれた曲をプレイリストとして<br />Spotifyにインポートしました！</p>
       <div id="playlist_result" className="w-3/4 my-8 mx-auto flex overflow-scroll"></div>
+      <button className="bg-green-500 rounded-lg w-48 py-2 px-4" onClick={() => tweet()}>トップへ戻る</button>
       <a href="/" className="bg-green-500 rounded-lg w-48 py-2 px-4">トップへ戻る</a>
     </div>
   );
