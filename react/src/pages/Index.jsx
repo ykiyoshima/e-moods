aff2168ca8f11fb85cf53abb9144718247c446e0
ykiyoshima <y.kiyoshima.271@gmail.com>
2022-04-08 15:36:44 +0900

    jquery.ripples installed

--- react/src/pages/Index.jsx
+++ react/src/pages/Index.jsx
@@ -2,7 +2,8 @@ import axios from "axios";
 import { useDropzone } from 'react-dropzone';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faImage } from "@fortawesome/free-regular-svg-icons";
-import Ripples from "react-ripples";
+import $ from "jquery";
+
 
 export const Index = () => {
   axios.get('/index', { withCredentials: true })
@@ -55,6 +56,7 @@ export const Index = () => {
         }
       });
     refreshToken();
+    $('.bg-green-500').ripples();
   }
 
   const token = async () => {
@@ -120,9 +122,7 @@ export const Index = () => {
       <h1 className="text-5xl font-bold pt-24 pb-16">e-moods</h1>
       <p><span id="username"></span>の顔写真を送信することで<br/>写真から感情を分析しその結果に基づいて<br/>あなたにぴったりな3曲を選びます！</p>
       <div id="signin_btn" className="my-8"></div>
-      <Ripples>
         <a href="/setting" className="bg-green-500 rounded-lg inline-block w-48 h-10 align-middle py-2">設定アーティスト変更</a>
-      </Ripples>
       <div {...getRootProps({ style })}>
         <input {...getInputProps()} />
         <FontAwesomeIcon className="text-6xl mt-6 mb-4" icon={faImage} />
@@ -134,9 +134,7 @@ export const Index = () => {
       </div>
       <div id="image_area" className="my-8 w-full"></div>
       <div id="message"></div>
-      <Ripples>
         <button id="make_recommendations" className="bg-green-500 rounded-lg inline-block w-48 h-10 align-middle py-2" onClick={() => startEmotionAnalysis()}>感情分析を開始</button><br/><br/>
-      </Ripples>
     </div>
   );
 };
